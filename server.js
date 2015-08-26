165a263d75bdce3d9508e9e00955314930d50f87
gino0012 <gino_0012@yahoo.com.ph>
2015-08-26 11:34:32 +0800

    server can return single site with limited set of properties identified by id

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ var router = express.Router();              // get an instance of the express Ro
 // middleware to use for all requests
 router.use(function(req, res, next) {
     // do logging
-    console.log('Something is happening.');
+    console.log('Something is happening.'); 
     next(); // make sure we go to the next routes and don't stop here
 });
 
